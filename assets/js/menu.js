70fca3fce72fb25801a2ddabe4e5f578a3e3ea73
sharawaa <115978040+sharawaa@users.noreply.github.com>
2023-09-06 09:06:29 +0000

    added contact side nenu

---
+++ assets/js/menu.js
@@ -0,0 +1,42 @@
+const test = document.querySelector(".toggle-icon");
+const test2 = document.querySelector(".nav-container");
+const main= document.querySelector(".body")
+const arrowButton = document.querySelector(".arrowButton")
+const arrow= document.querySelector(".arrow")
+test.addEventListener("click", function () {
+  test2.classList.toggle("pushed");
+  main.classList.toggle("active");
+});
+
+arrowButton.addEventListener("click", () => {
+  arrow.classList.toggle("activeArrow")
+  var targetY = 1004;
+  var currentY = window.scrollY;
+  console.log(currentY);
+
+  if (currentY >= 0 && currentY < 900) {
+  
+    window.scrollTo({
+      top: currentY + targetY,
+      behavior: "smooth" 
+    });
+  } else if (currentY >= 900) {
+  
+    window.scrollTo({
+      top: currentY - targetY,
+      behavior: "smooth" 
+    });
+  }
+});
+
+
+
+
+
+
+// function(){
+//   
+//   console.log(window.scrollY);
+  
+  
+//   }
